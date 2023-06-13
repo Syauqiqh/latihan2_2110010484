0d75e6bda0de37775edd7b23d1f4a5c2f043562b
Syauqiqh <shauqikhairi@gmail.com>
2023-06-13 09:43:05 +0800

    melengkapi class Mahasiswa

--- universitas/Mahasiswa.java
+++ universitas/Mahasiswa.java
@@ -5,6 +5,36 @@ package universitas;
 public class Mahasiswa {
     String npm, nama, prodi;
 
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
     public String getDetail() {
       System.out.println("nama \t: "+ nama);
       System.out.println("npm \t: "+ npm);
